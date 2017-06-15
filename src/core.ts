98a7ff81ca90e828aa60099a856af6ed27087db5
DomDom <dom.dumont@gmail.com>
2017-06-15 17:04:50 +0200

    Add a test

--- src/core.ts
+++ src/core.ts
@@ -4,13 +4,12 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
-function getWinners(vote) 
-{
+function getWinners(vote) {
   if (!vote) return [];
-  let tempList:any = vote.get('pair');
+  let tempList: any = vote.get('pair');
   let a = tempList.get(0);
   let b = tempList.get(1);
- 
+
 
   /*
   if (!vote) return [];
@@ -20,25 +19,31 @@ function getWinners(vote)
   */
   const aVotes = vote.getIn(['tally', a], 0);
   const bVotes = vote.getIn(['tally', b], 0);
-  console.log('aVotes = '+ aVotes);
-  console.log('bVotes = '+ bVotes);
+  console.log('aVotes = ' + aVotes);
+  console.log('bVotes = ' + bVotes);
   if (aVotes > bVotes) return [a];
   else if (aVotes < bVotes) return [b];
-  else 
-  {
+  else {
     console.log("les 2 sont egaux");
     return [a, b];
   }
-  
+
 }
 
 export function next(state) {
   const entries = state.get('entries')
     .concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({ pair: entries.take(2) }),
-    entries: entries.skip(2)
-  });
+
+  if (entries.size === 1) {
+    return state.remove('vote')
+      .remove('entries')
+      .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({ pair: entries.take(2) }),
+      entries: entries.skip(2)
+    });
+  }
 }
 
 
