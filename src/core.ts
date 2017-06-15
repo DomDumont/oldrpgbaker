6f241429100418b028d7fa10d72ed7eb308527e6
DomDom <dom.dumont@gmail.com>
2017-06-15 11:58:56 +0200

    Now I can debug tests and all tests are ok

--- src/core.ts
+++ src/core.ts
@@ -1,5 +1,52 @@
-import {List} from 'immutable';
+import { List, Map } from 'immutable';
 
 export function setEntries(state, entries) {
   return state.set('entries', List(entries));
+}
+
+function getWinners(vote) 
+{
+  if (!vote) return [];
+  let tempList:any = vote.get('pair');
+  let a = tempList.get(0);
+  let b = tempList.get(1);
+ 
+
+  /*
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  console.log(a);
+  console.log(b);
+  */
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  console.log('aVotes = '+ aVotes);
+  console.log('bVotes = '+ bVotes);
+  if (aVotes > bVotes) return [a];
+  else if (aVotes < bVotes) return [b];
+  else 
+  {
+    console.log("les 2 sont egaux");
+    return [a, b];
+  }
+  
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+    .concat(getWinners(state.get('vote')));
+  return state.merge({
+    vote: Map({ pair: entries.take(2) }),
+    entries: entries.skip(2)
+  });
+}
+
+
+
+export function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  );
 }
